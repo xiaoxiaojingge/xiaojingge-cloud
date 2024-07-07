b8b84c29129b4f6ad3ecfc201b10554b38880fc0
xiaojingge <2427259171@qq.com>
2024-07-07 16:41:52 +0800

    feat: ✨ 初始化网关模块，引入spring-javaformat格式化代码

---
+++ xiaojingge-cloud-gateway/src/main/java/cloud/xiaojingge/gateway/XiaojinggeCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package cloud.xiaojingge.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaojinggeCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaojinggeCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ xiaojingge-cloud-gateway/src/test/java/cloud/xiaojingge/gateway/XiaojinggeCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xiaojingge.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaojinggeCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
